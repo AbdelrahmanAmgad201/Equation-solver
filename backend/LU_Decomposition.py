9d6b3009d7eef3c58809565100e650776eb382f5
mohamedelsayed0001 <mohamedelsayyed234@gmail.com>
2024-11-22 17:06:48 +0200

    base

---
+++ backend/LU_Decomposition.py
@@ -0,0 +1,33 @@
+from .parser import Parser
+import numpy as np
+from sympy import Matrix as SympyMatrix
+
+class LU_Decomposition:
+    def __init__(self):
+        # Create an instance of Parser to handle input
+        self.parser = Parser()
+        self.matrixA = None
+        self.matrixB = None
+
+    def initialize_matrices(self):
+
+        # Use the parser to extract matrices
+        self.matrixA = self.parser.get_matrixA()
+        self.matrixB = self.parser.get_matrixB()
+
+    def solve(self):
+        if isinstance(self.matrixA, np.ndarray):
+            return self._solve_numerical()
+        elif isinstance(self.matrixA, SympyMatrix):
+            return self._solve_symbolic()
+
+    def _solve_Numerical(self):
+        """
+            Solves the system numerically using NumPy operations.
+        """
+        pass
+    def _solve_Symbolic(self):
+        """
+            Solves the system symbolically using SymPy operations.
+        """
+        pass
